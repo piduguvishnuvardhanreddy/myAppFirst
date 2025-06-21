5254d47a95551236c3a1b493a1c832a252c7f1ad
piduguvishnuvardhanreddy <pidugu.vishnu2@gmail.com>
2025-06-21 11:31:57 +0530

    First Commit

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 // MODELS
 const userSchema = new mongoose.Schema({
   name: String,
